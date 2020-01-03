49f1228e1e290644c34ef44bb963b18403359dad
HUAWEI <HUAWEI@I-do>
2020-01-03 12:00:16 +0800

    2

--- _test/src/_test/Test1.java
+++ _test/src/_test/Test1.java
@@ -1,13 +1,19 @@
 package _test;
 
 public class Test1 {
- 
+	
+	public static void main(String[] args) {
+		int c =21;
+
+		String s = "abc";
+		int b =22;
+
+		System.out.println(s);
+		
+	}
 	
 
 	
-	int c =21;
 
-	String s = "abc";
-	int b =22;
 
 }
